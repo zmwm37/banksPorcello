22cbf79c4c1b4aa609a3c526cf2562d124e1609a
zmwm37 <z37meitus@gmail.com>
2021-04-21 10:44:41 -0500

    add star rating and color removal features

--- chapter-06/colors-app/src/StarRating.js
+++ chapter-06/colors-app/src/StarRating.js
@@ -1,21 +1,26 @@
 import React, { useState } from 'react';
-import {FaMandalorian} from 'react-icons/fa'; // font awesome star from react icon library
+import {FaStar} from 'react-icons/fa'; // font awesome star from react icon library
 
 // component for individual star
 const Star = ({ selected = false, onSelect = f => f}) => (
-  <FaMandalorian color = { selected ? 'red' : 'grey'} onClick = {onSelect} />
+  <FaStar color = { selected ? 'red' : 'grey'} onClick = {onSelect} />
 );
 
 // function that takes length as arg and creates array of that length
 const createArray = length => [...Array(length)];
 
-export default function StarRating ({ totalStars = 5, selectedStars = 0 }) {
+export default function StarRating ({
+  totalStars = 5,
+  selectedStars = 0,
+  onRate = f => f
+}) {
   return (
     <>
       {createArray(totalStars).map((n, i) => (
         <Star
           key = {i}
           selected = {selectedStars > i}
+          onSelect = {() => onRate(i + 1)}
         />
       )) }
       <p>
