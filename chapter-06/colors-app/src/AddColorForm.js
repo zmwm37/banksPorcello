409b4bb2cc1598101cbc10b6529732580159aaa2
zmwm37 <z37meitus@gmail.com>
2021-04-22 12:50:01 -0500

    refactor to add context hooks 🤯

--- chapter-06/colors-app/src/AddColorForm.js
+++ chapter-06/colors-app/src/AddColorForm.js
@@ -1,14 +1,16 @@
-import Redact, { useState } from "react";
+import React, { useState } from "react";
 import { useInput } from './hooks/useInput.js'
+import { useColors } from './color-hooks.js'
 
 export default function AddColorForm({ onNewColor = f => f }) {
   const [titleProps, resetTitle] = useInput("");
-  const [colorProps, resetColor] = useInput("#000000")
+  const [colorProps, resetColor] = useInput("#000000");
+  const { addColor } = useColors();
 
   const submit = e => {
     e.preventDefault();
     //values to parent
-    onNewColor(titleProps.value, colorProps.value);
+    addColor(titleProps.value, colorProps.value);
     //reset values after passing to parent
     resetTitle("");
     resetColor("");
--- chapter-06/colors-app/src/index.js
+++ chapter-06/colors-app/src/index.js
@@ -1,8 +1,11 @@
-import React from "react";
+import React, { createContext } from "react";
+import ColorProvider from "./color-hooks.js"
 import { render } from "react-dom";
 import App from "./App.js";
 
 render(
-  <App />,
+  <ColorProvider>
+    <App />
+  </ColorProvider>,
   document.getElementById("root")
 );
