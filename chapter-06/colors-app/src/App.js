8c31073bf5bd6b22ccb29830b3327802212440a2
Zander Meitus <zmeitus@deloitte.com>
2021-04-22 12:50:01 -0500

    refactor to add context hooks 🤯

--- chapter-06/colors-app/src/App.js
+++ chapter-06/colors-app/src/App.js
@@ -1,40 +1,14 @@
+// i didn't remove all the stuff beofre retrieving colors with useContext. come back after that section to see if I can replicate app.js from pg. 123
 import React, { useState } from "react";
-import colorData from "./color-data.json";
 import ColorList from "./ColorList.js";
 import AddColorForm from './AddColorForm.js';
 import { v4 } from 'uuid';
 
 export default function App() {
-  const [colors, setColors] = useState(colorData);
   return (
     <>
-      <AddColorForm
-        onNewColor = {(title, color) => {
-          const newColors = [
-            ...colors,
-            {
-              id: v4(),
-              rating: 0,
-              title,
-              color
-            }
-          ];
-          setColors(newColors);
-        }}
-      />
-      <ColorList
-        colors = {colors}
-        onRateColor = {(id,rating) => {
-          const newColors = colors.map(color =>
-            color.id === id ? {...color, rating} : color
-          );
-          setColors(newColors);
-        }}
-        onRemoveColor = {id => {
-          const newColors = colors.filter(color => color.id !== id);
-          setColors(newColors);
-        }}
-      />
+      <AddColorForm />
+      <ColorList />
     </>
 
   );
