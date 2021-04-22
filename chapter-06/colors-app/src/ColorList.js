0622ad7040bf056b078e6bc1279de2f05088b40f
zmwm37 <z37meitus@gmail.com>
2021-04-22 12:50:01 -0500

    refactor to add context hooks 🤯

--- chapter-06/colors-app/src/ColorList.js
+++ chapter-06/colors-app/src/ColorList.js
@@ -1,21 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 import Color from "./Color.js"
+import { useColors } from './color-hooks.js';
 
-export default function ColorList({
-  colors = [],
-  onRemoveColor = f => f,
-  onRateColor = f =>f
-}) {
-  if(!colors.length) return <div> No Colors Listed. (Add a color)</div>;
+export default function ColorList(){
+  const { colors } = useColors();
   return (
-    <div>
+    <div className = "color-list">
       {
         colors.map(color =>
           <Color
             key = {color.id}
             {...color}
-            onRemove = {onRemoveColor}
-            onRate = {onRateColor}
           />)
       }
     </div>
