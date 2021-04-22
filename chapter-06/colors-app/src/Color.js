e894a2f7df8ac3429276918b7de7e1c93491f788
zmwm37 <z37meitus@gmail.com>
2021-04-22 12:50:01 -0500

    refactor to add context hooks 🤯

--- chapter-06/colors-app/src/Color.js
+++ chapter-06/colors-app/src/Color.js
@@ -1,24 +1,24 @@
 import React from "react";
 import StarRating from "./StarRating.js";
 import {FaTrash} from "react-icons/fa"
+import { useColors } from './color-hooks.js'
 
 export default function Color({
    id,
    title,
    color,
-   rating,
-   onRemove = f => f,
-   onRate = f => f}) {
+   rating}) {
+  const { rateColor, removeColor } = useColors();
   return (
     <section key = {color}>
       <h1>{title}</h1>
-      <button onClick = { () => onRemove(id) }>
+      <button onClick = { () => removeColor(id) }>
         <FaTrash />
       </button>
       <div style = {{ height: 50, backgroundColor: color }} />
       <StarRating
         selectedStars = {rating}
-        onRate = {rating => onRate(id, rating)}
+        onRate = {rating => rateColor(id, rating)}
        />
     </section>
   );
